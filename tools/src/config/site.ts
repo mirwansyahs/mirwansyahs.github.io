eb563264baec6f3889780cc368a8c00714f5f2f9
Irwansyah <ext-m.irwansyah@mandiriinhealth.co.id>
2026-03-10 14:12:49 +0700

    add tools

--- tools/src/config/site.ts
+++ tools/src/config/site.ts
@@ -1,14 +1,27 @@
 export const SITE = {
     name: "Irwansyah Tools",
-    title: "Irwansyah Tools - JSON, Regex, JWT, UUID, Timestamp, Base64, URL/HTML Encode Decode",
+    title: "Irwansyah Tools - DNS, MX, A, CNAME, TXT, CAA, SRV, SPF, DKIM, DMARC",
     description:
-        "Koleksi tools gratis: JSON formatter validator, regex tester, JWT decoder, UUID generator, timestamp converter, Base64, URL encode/decode, HTML encode/decode, AES encrypt/decrypt, hash generator.",
+        "Koleksi tools gratis: DNS lookup, DNS propagation checker, MX/A/NS/CNAME/TXT/CAA/SRV checker, SPF checker, DKIM checker, DKIM generator, DMARC checker, JSON formatter validator, regex tester, JWT decoder, UUID generator, timestamp converter, Base64, URL encode/decode, HTML encode/decode, AES encrypt/decrypt, hash generator.",
     url: "https://tools.irwansyah.my.id",
     locale: "id_ID",
     twitter: "@mirwansyahs"
 };
 
 export const NAV_ITEMS = [
+    { label: "DNS Propagation", href: "/tools/dns-propagation" },
+    { label: "DNS Lookup", href: "/tools/dns-lookup" },
+    { label: "MX Lookup", href: "/tools/mx-lookup" },
+    { label: "A Lookup", href: "/tools/a-lookup" },
+    { label: "CAA Checker", href: "/tools/caa-checker" },
+    { label: "SRV Checker", href: "/tools/srv-checker" },
+    { label: "CNAME Lookup", href: "/tools/cname-lookup" },
+    { label: "TXT Lookup", href: "/tools/txt-lookup" },
+    { label: "NS Lookup", href: "/tools/ns-lookup" },
+    { label: "SPF Checker", href: "/tools/spf-checker" },
+    { label: "DKIM Checker", href: "/tools/dkim-checker" },
+    { label: "DKIM Generator", href: "/tools/dkim-generator" },
+    { label: "DMARC Checker", href: "/tools/dmarc-checker" },
     { label: "JSON Formatter/Validator", href: "/tools/json-formatter-validator" },
     { label: "JSON Viewer", href: "/tools/json-viewer" },
     { label: "Regex Tester", href: "/tools/regex-tester" },
